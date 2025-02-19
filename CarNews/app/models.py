a639fb25ad03472869a7c497b7d4edac3055e261
Aziret <sharshenbaevaziret@gmail.com>
2025-02-19 15:01:43 +0600

    initial commit

---
+++ CarNews/app/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+
+class Category(models.Model):
+    title = models.CharField(max_length=123)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'категория'
+        verbose_name_plural = 'категории'
+
+
+class Color(models.Model):
+    title = models.CharField(max_length=123)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'цвет'
+        verbose_name_plural = 'цвета'
+
+
+
+class Car(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    title = models.CharField(max_length=123)
+    model = models.CharField(max_length=123)
+    year = models.SmallIntegerField()
+    color = models.ForeignKey(Color, on_delete=models.PROTECT)
+    image = models.ImageField(upload_to='media/car_images')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'машина'
+        verbose_name_plural = 'машины'
+
+
